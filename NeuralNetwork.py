86e4d004e0cf25c0585cebb23ddfb86673bf89e5
jb <jbwolf@hotmail.fr>
2020-10-03 19:52:06 +0200

    go

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -12,16 +12,14 @@ class NeuralNetwork:
     def generate_weights(self):
         self.weights = []
         for i in range(len(self.schema)-1):
-            matrice = np.random.random(size=(self.schema[i]+1, self.schema[i+1]))
+            matrice = np.random.random(size=(self.schema[i+1], self.schema[i]+1))
             self.weights.append(matrice)
 
     def run(self, ndinput):
-        result = np.column_stack((ndinput, np.array([[1]])))
-        for i in range(len(self.weights)-1):
-            result = np.column_stack(
-                (self.activation[i](np.dot(result, self.weights[i])), np.array([[1]]))
-            )
-        return self.activation[-1](np.dot(result, self.weights[-1]))
+        result = ndinput
+        for i in range(len(self.weights)):
+            result = self.activation[i](np.dot(self.weights[i], result))
+        return result
 
     def train(self, ndinput, ndoutput):
         n = len(self.schema)
@@ -30,31 +28,26 @@ class NeuralNetwork:
         E = [] # Perte
         # == Propagation avant avec stockage des résultats intermediaire pour calcul de perte ==
         #Premier couche avec biais pour initialiser
-        result = np.column_stack((ndinput, np.array([[1]])))
+        result = ndinput
         X.append(result)
-        for i in range(len(self.weights) - 1): # Les n-2 couches suivante (avec le biais)
-            h = np.dot(result, self.weights[i])
-            result = np.column_stack((self.activation[i](h), np.array([[1]])))
-            print(result)
-            H.append(h)
+        for i in range(len(self.weights)):
+            h = np.dot(self.weights[i], result)
+            result = self.activation[i](h)
             X.append(result)
-        # Derniere couche sans biais en sortie
-        h = np.dot(result, self.weights[-1])
-        H.append(h)
-        X.append(self.activation[-1](h))
+            H.append(h)
         # == Calcul des pertes ==
-        e = np.dot(np.transpose(self.derivation_activation[-1](H[-1])), (X[-1] - ndoutput)) # derniere couche
+        e = self.derivation_activation[-1](H[-1]) * (X[-1] - ndoutput) # * car c est pour chaque ligne i
         E.append(e)
+
         for i in range(n-2): # On aura n-1 matrice E, on a deja fait celle n-1, il nosu en reste n-2
             ni = n-2+i # coeff inverse car on va a l envers (on utilisera i pour E car on le retournera a la fin
-            e = self.derivation_activation[ni-1](H[ni-1]) * (np.dot(self.weights[ni], E[i]))
+            e = self.derivation_activation[ni-1](H[ni-1]) * (np.dot(np.transpose(self.weights[ni]), E[i]))
             E.append(e)
         E.reverse()
         # == Modification des poids ==
-        """
+
         for l in range(n-1):
-            self.weights[l] = self.weights[l] - self.learning_rate * np.dot(E[l], X[l])
-        """
-        return X, H, E
+            self.weights[l] = self.weights[l] - self.learning_rate * np.dot(E[l], np.transpose(X[l]))
+
 
 
--- main.py
+++ main.py
@@ -5,13 +5,26 @@ from NeuralNetwork import NeuralNetwork
 sigmoid = lambda x: 1/(1+np.exp(-x))
 dsigmoid = lambda x: np.exp(-x)/np.power(np.exp(-x)+1, 2)
 
-n = NeuralNetwork([2, 2, 2], 0.01, [sigmoid, sigmoid], [dsigmoid, dsigmoid])
-t = np.random.random(size=(1, 2))
-tr = np.random.random(size=(1, 2))
-r = n.run(t)
+n = NeuralNetwork([2, 4, 1], 0.01, [sigmoid, sigmoid], [dsigmoid, dsigmoid])
+DATA = {
+    "(0,0)" : (np.array([[0], [0]]), np.array([[0]])),
+    "(1,0)" : (np.array([[1], [0]]), np.array([[1]])),
+    "(0,1)" : (np.array([[0], [1]]), np.array([[1]])),
+    "(1,1)" : (np.array([[1], [1]]), np.array([[0]])),
+}
+def afficher_test():
+    for k, i in DATA.items():
+        r = n.run(i[0])
+        print(k, " -> ", r[0, 0])
 
-print(t, " ->", r, "(test aleatoire)")
+def train_test():
+    for k, i in DATA.items():
+        n.train(*i)
 
-x, h, e = n.train(t,tr)
-print("finished")
+print("== Resultat avant entrainement ==")
+afficher_test()
+for i in range(1):
+    train_test()
+print("== Resultat apres entrainement ==")
+afficher_test()
 
