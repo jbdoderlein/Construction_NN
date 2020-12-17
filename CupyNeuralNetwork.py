5f742d94767cd831d7651e909a12567f3d1d078f
jb <jbwolf@hotmail.fr>
2020-12-17 13:55:16 +0100

    cupy version

---
+++ CupyNeuralNetwork.py
@@ -0,0 +1,120 @@
+import cupy as np
+import dill
+import time
+
+class NeuralNetwork:
+    """"Réseaux de neurones"""
+    def __init__(self, schema=[], learning_rate=0.1, activation=[], derivation_activation=[], load=None):
+        if not load:
+            self.schema = schema
+            self.activation = activation
+            self.learning_rate = learning_rate
+            self.derivation_activation = derivation_activation
+            self.generate_weights()
+            self.generate_biases()
+        else:
+            self.load(load)
+
+    def save(self, filename):
+        data = {
+            'schema': self.schema,
+            'activation': self.activation,
+            'derivation_activation': self.derivation_activation,
+            'learning_rate': self.learning_rate,
+            'weights': self.weights,
+            'biases': self.biases
+        }
+        with open(filename, 'wb') as f:
+            dill.dump(data,f)
+
+    def load(self, filename):
+        with open(filename, 'rb') as f:
+            nn = dill.load(f)
+        self.schema = nn["schema"]
+        self.activation = nn["activation"]
+        self.learning_rate = nn["learning_rate"]
+        self.derivation_activation = nn["derivation_activation"]
+        self.weights = nn["weights"]
+        self.biases = nn["biases"]
+
+    def generate_weights(self):
+        self.weights = []
+        for i in range(len(self.schema)-1):
+            matrice = np.random.normal(0, 1, size=(self.schema[i+1], self.schema[i]))
+            self.weights.append(matrice)
+
+    def generate_biases(self):
+        self.biases = []
+        for i in range(len(self.schema)-1):
+            matrice = np.zeros(shape=(self.schema[i+1], 1))
+            self.biases.append(matrice)
+
+    def forward_propagation(self, vector_input):
+        result = vector_input.reshape(vector_input.shape[0], 1) # on met en colonne
+        for i in range(len(self.weights)):
+            result = self.activation[i](np.dot(self.weights[i], result) + self.biases[i])
+        return result
+
+    def train(self, train_data, train_labels):
+        s = len(self.weights)  # Nombre de matrice de poids (et aussi de biais)
+        m = len(train_data)  # Taille du set d'entrainement
+        dW = [0]*s
+        dB = [0]*s
+        for i in range(m):
+            X = []
+            Z = []
+            #Propagation avant
+            result = train_data[i].reshape(train_data[i].shape[0], 1)
+            X.append(result)
+            for j in range(s):
+                z = np.dot(self.weights[j], result) + self.biases[j]
+                result = self.activation[j](z)
+                X.append(result)
+                Z.append(z)
+            #Retro propagation
+            dz = X[-1] - train_labels[i]
+            dW[-1] += np.dot(dz, X[-2].T)
+            dB[-1] += dz
+            for j in range(2, s+1):
+                da = np.dot(self.weights[-j+1].T, dz)
+                dz = np.multiply(da, self.derivation_activation[-j](X[-j]))
+                dW[-j] = dW[-j] + np.dot(dz, X[-j-1].T)
+                dB[-j] = dB[-j] + dz
+        #Application aux biais et aux poids
+        for j in range(s):
+            self.biases[j] -= self.learning_rate * (dB[j] / m)
+            self.weights[j] -= self.learning_rate * (dW[j] / m)
+
+    def losses(self, training_data, training_labels):
+        """
+        Erreur quadratique moyenne
+        :param training_data: liste de tests
+        :param training_labels: resultats attendu des test
+        :return: erreur quadratique moyenne
+        """
+        loss = 0
+        n = len(training_data)
+        for i in range(n):
+            result = self.forward_propagation(training_data[i])
+            loss += np.mean((result - training_labels[i]) ** 2)
+        return loss/n
+
+    def cross_entropy(self, p, q):
+        return -sum(p * np.log(q))
+
+    def loss_cross_entropy(self, training_data, training_labels):
+        """
+        Erreur quadratique moyenne
+        :param training_data: liste de tests
+        :param training_labels: resultats attendu des test
+        :return: erreur quadratique moyenne
+        """
+        loss = 0
+        n = len(training_data)
+        for i in range(n):
+            result = self.forward_propagation(training_data[i])
+            loss += np.mean([self.cross_entropy(result, training_labels[i]), self.cross_entropy(1 - result, 1 - training_labels[i])])
+        return loss/n
+
+
+
---
+++ nn_mnist_cupy.py
@@ -0,0 +1,75 @@
+import mnist
+import cupy as np
+from matplotlib import pyplot as plt
+from NeuralNetwork import NeuralNetwork
+import time
+
+
+def convert_label(labels):
+    newlab = [0] * len(labels)
+    for i in range(len(labels)):
+        t = np.zeros(10)
+        t[labels[i]] = 1
+        newlab[i] = t[:, np.newaxis]
+    return np.array(newlab)
+
+
+def max_output(v):
+    maximum, imax = 0, 0
+    for i in range(len(v)):
+        if v[i] > maximum:
+            maximum, imax = v[i], i
+    return imax
+
+
+x_train, t_train, x_test, t_test = mnist.load()
+
+tjb_train = convert_label(t_train)
+tjb_test = convert_label(t_test)
+
+
+def img_show(array):
+    pixels = array.reshape((28, 28))
+    plt.imshow(pixels, cmap='gray')
+    plt.show()
+
+
+sigmoid = lambda x: 1 / (1 + np.exp(-x))
+dsigmoid = lambda x: x * (1 - x)
+tanh = np.tanh
+dtanh = lambda x: 1 - (np.tanh(x) ** 2)
+
+if __name__ == '__main__':
+    T1 = time.time()
+    n = NeuralNetwork([784, 950, 500, 150, 10], 0.01, [tanh, sigmoid, tanh, sigmoid], [dtanh, dsigmoid, dtanh, dsigmoid])
+
+    BATCH = 100
+    BATCH_SIZE = 10
+    losses = np.zeros(BATCH)
+    losses2 = np.zeros(BATCH)
+
+    for i in range(BATCH):
+        t1 = time.time()
+        for j in range(BATCH_SIZE):
+            rn = np.random.randint(59999)
+            n.train([x_train[rn]], [tjb_train[rn]])
+        t2 = time.time()
+        u = n.losses(x_test[:10], tjb_test[:10])
+        losses[i] = u
+        t3 = time.time()
+        u = n.loss_cross_entropy(x_test[:10], tjb_test[:10])
+        losses2[i] = u
+        t4 = time.time()
+        print(f"Batch : {i + 1}/{BATCH} en {round(t2 - t1, 4)} s avec {round(t3 - t2, 4)} en loss et {round(t4 - t3, 4)} en loss pool")
+    T2 = time.time()
+    print("Le tout en ", T2-T1)
+    #n.save("mninst_data_1")
+    """
+    plt.figure()
+    #plt.plot(losses)
+    plt.plot(losses2)
+    plt.xlabel("BATCH")
+    plt.ylabel("Loss")
+    plt.title(f"MNIST Loss with {BATCH} batchs of {BATCH_SIZE} retropopagation")
+    plt.show()
+"""
